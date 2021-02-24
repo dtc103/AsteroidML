e37bd70b576aa21a99fc3f736c5760d3b47c4c82
Kerner, Jan <Jan.Kerner@vector.com>
2021-02-24 15:19:47 +0100

    .

--- AsteroidML/src/main.rs
+++ AsteroidML/src/main.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 use rand::{Rng, thread_rng};
 use std::collections::HashMap;
+use std::ops::{Index, IndexMut};
 
 
 #[derive(Debug)]
@@ -22,7 +23,7 @@ enum Moves{
 struct GridWorld{
     x_length: u32,
     y_length: u32,
-    grids: Vec<Grid>,
+    grids: Vec<GameObjectTypes>,
 }
 impl GridWorld{
     fn new(x_size: u32, y_size: u32) -> Self{
@@ -30,36 +31,43 @@ impl GridWorld{
 
         for y in 0..y_size{
             for x in 0..x_size{
-                tmp_grid.push(Grid{x: x, y: y, content: GameObjectTypes::Empty});
+                tmp_grid.push(GameObjectTypes::Empty);
             }
         }
         GridWorld{x_length: x_size, y_length: y_size, grids: tmp_grid}
     }
-    fn get_grid(&mut self, x: usize, y: usize) -> Result<&mut Grid, String>{
+    fn get_grid(&mut self, x: usize, y: usize) -> Result<GameObjectTypes, String>{
         if x >= self.x_length as usize || y >= self.y_length as usize{
             return Err(String::from("dimensions do not fit"));
         }
 
-        Ok(&mut self.grids[(x + y * self.x_length as usize) as usize])
+        Ok(self.grids[(x + y * self.x_length as usize) as usize])
     }
 }
+impl Index<usize> for GridWorld{
+    type Output = GameObjectTypes;
+
+    fn index(&self, index: usize) -> &Self::Output{
+        &GameObjectTypes::Asteroid
+    }
+
+}
+impl IndexMut<usize> for GridWorld{
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut GameObjectTypes::Asteroid
+    }
+}
+
 impl fmt::Display for GridWorld{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
         for y in 0..self.y_length{
             for x in 0..self.x_length{
-                write!(f, "{:?}\t", self.grids[(x + y*self.x_length) as usize].content);
+                write!(f, "{:?}\t", self.grids[(x + y*self.x_length) as usize]);
             }
             write!(f, "\n");
         }
         Ok(())
     }
-}
-
-#[derive(Debug)]
-struct Grid{
-    x: u32,
-    y: u32,
-    content: GameObjectTypes,
 }
 
 #[derive(Debug)]
@@ -82,22 +90,22 @@ fn main() {
 }
 
 fn init_grid_world(grid_world: &mut GridWorld){
-    grid_world.get_grid(0, 0).unwrap().content = GameObjectTypes::Asteroid;
-    grid_world.get_grid(1, 0).unwrap().content = GameObjectTypes::Asteroid;
-    grid_world.get_grid(3, 0).unwrap().content = GameObjectTypes::Asteroid;
+    grid_world.get_grid(0, 0).unwrap() = GameObjectTypes::Asteroid;
+    grid_world.get_grid(1, 0).unwrap() = GameObjectTypes::Asteroid;
+    grid_world.get_grid(3, 0).unwrap() = GameObjectTypes::Asteroid;
     
-    grid_world.get_grid(2, 1).unwrap().content = GameObjectTypes::Asteroid;
+    grid_world.get_grid(2, 1).unwrap() = GameObjectTypes::Asteroid;
     
-    grid_world.get_grid(4, 2).unwrap().content = GameObjectTypes::Asteroid;
-    grid_world.get_grid(5, 2).unwrap().content = GameObjectTypes::Asteroid;
+    grid_world.get_grid(4, 2).unwrap() = GameObjectTypes::Asteroid;
+    grid_world.get_grid(5, 2).unwrap() = GameObjectTypes::Asteroid;
     
-    grid_world.get_grid(2, 3).unwrap().content = GameObjectTypes::Asteroid;
-    grid_world.get_grid(5, 3).unwrap().content = GameObjectTypes::Asteroid;
+    grid_world.get_grid(2, 3).unwrap() = GameObjectTypes::Asteroid;
+    grid_world.get_grid(5, 3).unwrap() = GameObjectTypes::Asteroid;
 
-    grid_world.get_grid(0, 4).unwrap().content = GameObjectTypes::Asteroid;
-    grid_world.get_grid(3, 4).unwrap().content = GameObjectTypes::Asteroid;
+    grid_world.get_grid(0, 4).unwrap() = GameObjectTypes::Asteroid;
+    grid_world.get_grid(3, 4).unwrap() = GameObjectTypes::Asteroid;
 
-    grid_world.get_grid(5, 0).unwrap().content = GameObjectTypes::Goal;
+    grid_world.get_grid(5, 0).unwrap() = GameObjectTypes::Goal;
 }
 
 fn get_grid_world_value(grid_world: &GridWorld) -> Vec<f64>{
